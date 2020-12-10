456d5967a0859ba351e9f3d6d2c7329272da669e
ggabimoran <gabrielmoran7@live.fr>
2020-12-10 15:22:51 +0100

    corrected matrix interface

--- LookbackOption/lookbackoption_stat/matrix.h
+++ LookbackOption/lookbackoption_stat/matrix.h
@@ -7,10 +7,12 @@ namespace lookback {
 	class Matrix {
 	private:
 		int N_, M_;
-		std::vector<std::vector<double>> data;
+		std::vector<double> data; //access i,j element with data[i*N + j]
 	public:
 		//if normalInit true initialize with a standard normal distribution simulation for each component
 		Matrix(int N, int M, bool normalInit=true); 
+		double operator()(int i, int j) const;
+		double& operator()(int i, int j);
 	};
 }
 
